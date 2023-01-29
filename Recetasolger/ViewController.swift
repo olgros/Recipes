2b51e23c32a0ea647146ef97db2d2eee35c7eca6
ROSERO Olger <olger.rosero@idemia.com>
2023-01-29 08:38:02 -0500

    Initial Commit

---
+++ Recetasolger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recetasolger
+//
+//  Created by Olger Rosero on 29/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
